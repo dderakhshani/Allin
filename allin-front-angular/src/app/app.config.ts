e107dc355388ea520b7456772b4b9c2fe2c1d073
dderakhshani <d.derakhshani@yahoo.com>
2024-12-29 23:08:55 +0330

    fixed httpClient and API_BASE_URL issue

--- allin-front-angular/src/app/app.config.ts
+++ allin-front-angular/src/app/app.config.ts
@@ -4,12 +4,16 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { API_BASE_URL } from './core/services/base.http.service';
+import { environment } from '../environments/environment';
+import { provideHttpClient } from '@angular/common/http';
 
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
-    provideAnimationsAsync(),
-  
-  ]
+    providers: [
+        provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+        provideAnimationsAsync(),
+        provideHttpClient(),
+        { provide: API_BASE_URL, useValue: `${environment.baseUrl}/admin/api` },
+    ]
 };
