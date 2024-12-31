d04869160085193544bf07d76d450af3b9a37a6e
hamidardakani <hamidardakani71@gmail.com>
2024-12-31 21:54:13 +0330

    add person translate

--- allin-front-angular/src/app/subsystems/admin/components/add-employee/add-employee.component.ts
+++ allin-front-angular/src/app/subsystems/admin/components/add-employee/add-employee.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { BasicModule } from '../../../../core/basic.module';
+import { CalendarModule } from 'primeng/calendar';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { AccordionModule } from 'primeng/accordion';
 
 @Component({
   selector: 'app-add-employee',
   standalone: true,
-  imports: [],
+  imports: [BasicModule,CalendarModule,SelectButtonModule,AccordionModule,],
   templateUrl: './add-employee.component.html',
   styleUrl: './add-employee.component.scss'
 })
 export class AddEmployeeComponent {
+  value: string | undefined;
+  date: Date | undefined;
+
+  floatingOptions: any[] = [{ label: 'Floating', value2: 'Floating' }, { label: 'Fixed', value2: 'Fixed' }];
+    value2: string = 'Fixed';
 
 }
