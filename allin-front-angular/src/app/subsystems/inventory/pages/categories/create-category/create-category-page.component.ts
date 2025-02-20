cf85f33acf80a01ce73d6fbcca45eed305e41484
hamidardakani <hamidardakani71@gmail.com>
2025-02-20 14:15:53 +0330

    progress Create Category #63

---
+++ allin-front-angular/src/app/subsystems/inventory/pages/categories/create-category/create-category-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-category-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-category-page.component.html',
+  styleUrl: './create-category-page.component.scss'
+})
+export class CreateCategoryPageComponent {
+
+}
