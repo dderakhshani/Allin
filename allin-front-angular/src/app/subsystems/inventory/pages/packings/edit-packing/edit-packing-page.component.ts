c8e0fc3d06fb17af6f5fb9594169687cd4ec7c8b
hamidardakani <hamidardakani71@gmail.com>
2025-03-05 13:03:37 +0330

    progress Edit Packing #67

---
+++ allin-front-angular/src/app/subsystems/inventory/pages/packings/edit-packing/edit-packing-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-packing-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-packing-page.component.html',
+  styleUrl: './edit-packing-page.component.scss'
+})
+export class EditPackingPageComponent {
+
+}
