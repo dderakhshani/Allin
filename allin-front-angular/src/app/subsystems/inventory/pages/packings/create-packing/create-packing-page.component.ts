1deb217ad4cfbd77ac862cdd0470048d5650b216
hamidardakani <hamidardakani71@gmail.com>
2025-03-05 13:03:20 +0330

    progress Create Packing #66

---
+++ allin-front-angular/src/app/subsystems/inventory/pages/packings/create-packing/create-packing-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-packing-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-packing-page.component.html',
+  styleUrl: './create-packing-page.component.scss'
+})
+export class CreatePackingPageComponent {
+
+}
