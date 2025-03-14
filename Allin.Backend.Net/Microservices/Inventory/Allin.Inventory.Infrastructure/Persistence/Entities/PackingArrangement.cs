ff672addd838cab797cc6b715dc476414da2fc12
dderakhshani <d.derakhshani@yahoo.com>
2025-03-14 12:59:29 +0330

    removed ForeignKey annotation from PackingArrangement

--- Allin.Backend.Net/Microservices/Inventory/Allin.Inventory.Infrastructure/Persistence/Entities/PackingArrangement.cs
+++ Allin.Backend.Net/Microservices/Inventory/Allin.Inventory.Infrastructure/Persistence/Entities/PackingArrangement.cs
@@ -6,10 +6,8 @@ namespace Allin.Inventory.Infrastructure.Persistence;
 
 public class PackingArrangement : InventoryBaseEntity
 {
-    [ForeignKey("Packing")]
     public long PackingId { get; set; }
     public double ConversionFactor { get; set; }
-    [ForeignKey("ContainerPacking")]
     public long ContainerPackingId { get; set; }
     public virtual Packing ContainerPacking { get; set; } = null!;
     public virtual Packing Packing { get; set; } = null!;
